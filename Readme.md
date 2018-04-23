475a43d705d63ad6d4e1c6b4f8aaea3670446bda
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to add a custom popup menu item
+
+
+<p>This example demonstrates how to add a custom menu item (<i>"Hide this value"</i>) to the field value popup menu. In this example, the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxPivotGridASPxPivotGrid_PopupMenuCreatedtopic">ASPxPivotGrid.PopupMenuCreated</a> event is used to add a custom menu item, the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxPivotGridASPxPivotGrid_CustomCallbacktopic">ASPxPivotGrid.CustomCallback </a> event -- to process an item click on the server side, and the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxPivotGridScriptsASPxClientPivotGrid_PopupMenuItemClicktopic">ASPxClientPivotGrid.PopupMenuItemClick</a> client-side event is used to perform a callback to the server.</p><p><strong>See Also:</strong> <br />
+<a href="https://www.devexpress.com/Support/Center/p/E3909">How to show custom popup context menu items only for certain fields</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxPivotGrid
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+    Protected Sub ASPxPivotGrid1_PopupMenuCreated(ByVal sender As Object, _
+                           ByVal e As DevExpress.Web.ASPxPivotGrid.PivotPopupMenuCreatedEventArgs)
+        If e.MenuType = DevExpress.Web.ASPxPivotGrid.PivotGridPopupMenuType.FieldValueMenu Then
+            e.Menu.Items.Add("Hide this value", "hideValue")
+        End If
+    End Sub
+
+    Protected Sub ASPxPivotGrid1_CustomCallback(ByVal sender As Object, _
+                           ByVal e As DevExpress.Web.ASPxPivotGrid.PivotGridCustomCallbackEventArgs)
+        Dim pivot As ASPxPivotGrid = CType(sender, ASPxPivotGrid)
+        Dim parametrs() As String = e.Parameters.Split(New Char() {"|"c})
+        If parametrs.Length = 4 AndAlso parametrs(0) = "MenuItemClick" Then
+            If parametrs(1) = "hideValue" Then
+                Dim field As PivotGridField = pivot.Fields(parametrs(2))
+                If field IsNot Nothing Then
+                    field.FilterValues.Add(pivot.GetFieldValueByIndex(field, _
+                                                                      Convert.ToInt32(parametrs(3))))
+                End If
+            End If
+        End If
+
+    End Sub
+End Class
